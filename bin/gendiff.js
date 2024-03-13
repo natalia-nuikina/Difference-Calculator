773a53317affdb61599961e8696e7017a359f791
Наталья Нуйкина <me@nuikina.ru>
2024-03-13 18:12:55 +0300

    шаг два

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
-program.parse();
+  program.parse();
