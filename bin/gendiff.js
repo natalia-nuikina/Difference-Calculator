3bbe663af970d68c13e5b1bfd4bd254a7f8c91b6
Наталья Нуйкина <me@nuikina.ru>
2024-03-18 15:35:22 +0300

    застревание

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
+const path = require('node:path'); 
+const fs = require('fs');
+
+const a = (filepath1, filepath2) => {
+  // BEGIN
+  // const result = join(first, second, options.connector);
+  console.log(path.resolve(filepath1));
+  console.log(JSON.parse(fs.readFileSync(path.resolve(filepath1))));
+  console.log(JSON.parse(fs.readFileSync(path.resolve(filepath2))));
+  // console.log(fs.readFileSync(filepath2));
+  return path.resolve(filepath1)
+}
 
 program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action(a)
 
   program.parse();
