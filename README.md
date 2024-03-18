eeadba629bac248ae5f60f8796e6bc2d9553f9bc
Наталья Нуйкина <me@nuikina.ru>
2024-03-18 21:17:05 +0300

    шаг 4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/natalia-nuikina/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/natalia-nuikina/frontend-project-46/actions)
+
+### Asciinema:
+[![DEMO gendiff](https://asciinema.org/a/IMm8acHgcko63CvlwqivTVWs8.svg)](https://asciinema.org/a/IMm8acHgcko63CvlwqivTVWs8)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import path from 'node:path'; 
 import fs from 'fs';
+import _ from 'lodash';
+
+const getFile = (file) => JSON.parse(fs.readFileSync(path.resolve(file)));
 
 const gendiff = (filepath1, filepath2) => {
-  console.log(JSON.parse(fs.readFileSync(path.resolve(filepath1))));
-  console.log(JSON.parse(fs.readFileSync(path.resolve(filepath2))));
-  return;
+  const file1 = getFile(filepath1);
+  const file2 = getFile(filepath2);
+  const keys = _.union(Object.keys(file1), Object.keys(file2));
+  const sortedKeys = keys.sort((a, b) => a.localeCompare(b));
+  const tab = '  ';
+  let result = '{'
+  const b = sortedKeys.map((item) => {
+    if (Object.hasOwn(file1, item) && !Object.hasOwn(file2, item)) {
+      result += `\n${tab}- ${item}: ${file1[item]}`;
+      return item;
+    }
+    if (!Object.hasOwn(file1, item) && Object.hasOwn(file2, item)) {
+      result += `\n${tab}+ ${item}: ${file2[item]}`;
+      return item;
+    }
+    if (file1[item] === file2[item]) {
+      result += `\n${tab}${tab}${item}: ${file1[item]}`;
+      return item;
+    }
+    if (file1[item] !== file2[item]) {
+      result += `\n${tab}- ${item}: ${file1[item]}`;
+      result += `\n${tab}+ ${item}: ${file2[item]}`;
+      return item;
+    }
+  })
+
+result += '\n}'
+// console.log(result);
+// console.log(sortedKeys);
+// console.log(file1);
+
+
+  return result;
 }
 
 export default gendiff;
