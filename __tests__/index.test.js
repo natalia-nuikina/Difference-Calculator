6a84abcba7fa90b835e71f35370c7aca2da69709
Наталья Нуйкина <me@nuikina.ru>
2024-03-21 14:50:21 +0300

    Подключен Codeclimate

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,12 @@
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
+import { dirname, join } from 'path';
 import fs from 'fs';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff', () => {
