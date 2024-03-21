8171b2604daf72f4e68c768ea0cfc7cfbb17299f
Наталья Нуйкина <me@nuikina.ru>
2024-03-21 17:24:53 +0300

    Шаг 6: добавлены тесты

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,6 +9,14 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff', () => {
+test('gendiffJson', () => {
   expect(gendiff('file1.json', 'file2.json')).toBe(readFile('file.test.json'));
+});
+
+test('gendiffYml', () => {
+  expect(gendiff('filepath1.yml', 'filepath2.yml')).toBe(readFile('file.test.json'));
+});
+
+test('gendiffDifferrrent', () => {
+  expect(gendiff('filepath1.yml', 'file2.json')).toBe(readFile('file.test.json'));
 });
