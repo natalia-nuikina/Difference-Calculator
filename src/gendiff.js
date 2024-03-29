6d554f500aa0d3ff7fbef2fd44c9f1909fa953c0
Наталья Нуйкина <me@nuikina.ru>
2024-03-29 14:59:47 +0300

    Проверка

--- src/gendiff.js
+++ src/gendiff.js
@@ -14,15 +14,14 @@ const genDiff = (filePath1, filePath2) => {
       const currentValue1 = content1[key];
       const currentValue2 = content2[key];
       if (Object.hasOwn(content1, key) && !Object.hasOwn(content2, key)) {
-        result[key] = 'removed';
+        assignment(result, { [key]: 'removed' });
       } else if (!Object.hasOwn(content1, key) && Object.hasOwn(content2, key)) {
-        result[key] = 'added';
+        assignment(result, { [key]: 'added' });
       } else if (Object.hasOwn(content1, key) && Object.hasOwn(content2, key)) {
         if (_.isObject(currentValue1) && _.isObject(currentValue2)) {
-          const res = iter(currentValue1, currentValue2);
-          result[key] = res;
+          assignment(result, { [key]: iter(currentValue1, currentValue2) });
         } else if (currentValue1 === currentValue2) {
-          Object.assign(result, { [key]: 'unchanged' });
+          assignment(result, { [key]: 'unchanged' });
         } else if (currentValue1 !== currentValue2) {
           assignment(result, { [key]: 'updated' });
         }
@@ -32,27 +31,7 @@ const genDiff = (filePath1, filePath2) => {
     return result;
   };
   const diff = iter(currentContent1, currentContent2);
-// console.log(diff)
   return diff;
 };
 
 export default genDiff;
-
-
-// const genDiff = (obj1, obj2) => {
-//   const mergedObj = { ...obj1, ...obj2 };
-//   const result = {};
-//   for (const key of Object.keys(mergedObj)) {
-//     if (obj1[key] === obj2[key]) {
-//       Object.assign(result, { [key]: 'unchanged' });
-//     } else if (obj1[key] === undefined) {
-//       Object.assign(result, { [key]: 'added' });
-//     } else if (obj2[key] === undefined) {
-//       Object.assign(result, { [key]: 'deleted' });
-//     } else {
-//       Object.assign(result, { [key]: 'changed' });
-//     }
-//   }
-
-//   return result;
-// };
