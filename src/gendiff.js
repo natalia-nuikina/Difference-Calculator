7e7b6f9a61d682af89c62db56b3036e2cf6b49da
Наталья Нуйкина <me@nuikina.ru>
2024-04-02 21:33:22 +0300

    Правки

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,22 +5,20 @@ const genDiff = (currentContent1, currentContent2) => {
     const keys = _.union(Object.keys(content1), Object.keys(content2));
     const sortedKeys = keys.toSorted();
     const node = sortedKeys.map((key) => {
-      if (Object.hasOwn(content1, key) && !Object.hasOwn(content2, key)) {
+      if (!Object.hasOwn(content2, key)) {
         return { name: key, type: 'removed', value: content1[key] };
       }
-      if (!Object.hasOwn(content1, key) && Object.hasOwn(content2, key)) {
+      if (!Object.hasOwn(content1, key)) {
         return { name: key, type: 'added', value: content2[key] };
       }
-      if (Object.hasOwn(content1, key) && Object.hasOwn(content2, key)) {
-        if (_.isObject(content1[key]) && _.isObject(content2[key])) {
-          return { name: key, type: 'updatedInside', children: iter(content1[key], content2[key]) };
-        }
-        if (content1[key] === content2[key]) {
-          return { name: key, type: 'unchanged', value: content1[key] };
-        }
-        if (content1[key] !== content2[key]) {
-          return { name: key, type: 'updated', value: { file1: content1[key], file2: content2[key] } };
-        }
+      if (_.isPlainObject(content1[key]) && _.isPlainObject(content2[key])) {
+        return { name: key, type: 'updatedInside', children: iter(content1[key], content2[key]) };
+      }
+      if (_.isEqual(content1[key], content2[key])) {
+        return { name: key, type: 'unchanged', value: content1[key] };
+      }
+      if (!_.isEqual(content1[key], content2[key])) {
+        return { name: key, type: 'updated', value: { file1: content1[key], file2: content2[key] } };
       }
       throw new Error(`The key: '${key}' not found in files!`);
     });
--- src/parsers.js
+++ src/parsers.js
@@ -3,13 +3,12 @@ import yaml from 'js-yaml';
 import { readContent } from './helpers.js';
 
 const parseContent = (filePath) => {
-  const format = path.extname(filePath);
+  const format = path.extname(filePath).slice(1);
   switch (format) {
-    case '.json':
+    case 'json':
       return JSON.parse(readContent(filePath));
-    case '.yaml':
-      return yaml.load(readContent(filePath));
-    case '.yml':
+    case 'yaml':
+    case 'yml':
       return yaml.load(readContent(filePath));
     default:
       return JSON.parse(readContent(filePath));
