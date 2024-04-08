a866929dbb8f233b5100abd5dd08fbae0c1559df
Наталья Нуйкина <me@nuikina.ru>
2024-04-08 13:15:44 +0300

    Проверка 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,13 +7,15 @@ describe('genDiff', () => {
     ['file1.yml', 'file2.yml', 'stylish', 'stylish.test.txt'],
     ['file1.json', 'file2.yml', 'stylish', 'stylish.test.txt'],
     ['file1.json', 'file2.json', 'plain', 'plain.test.txt'],
-    ['file1.yml', 'file2.yml', 'plain', 'plain.test.txt'],
-    ['file1.json', 'file2.yml', 'plain', 'plain.test.txt'],
     ['file1.json', 'file2.json', 'json', 'json.test.json'],
-    ['file1.yml', 'file2.yml', 'json', 'json.test.json'],
-    ['file1.json', 'file2.yml', 'json', 'json.test.json'],
   ])('.genDiff', (fileName1, fileName2, formatName, fileName3) => {
     expect(start(getFixturePath(fileName1), getFixturePath(fileName2), formatName))
       .toBe(readContent(getFixturePath(fileName3)));
+  });
+});
+describe('default', () => {
+  test('default', () => {
+    expect(start(getFixturePath('file1.json'), getFixturePath('file2.json')))
+      .toBe(readContent(getFixturePath('stylish.test.txt')));
   });
 });
